f92891036f33441a51166fb0df7bc8ed0059f989
pankajnalawade8 <pankajnalawasw8@gmail.com>
2026-02-26 00:04:32 +0530

    new namuproject

---
+++ src/test/java/com/example/Namu/NamuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Namu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
